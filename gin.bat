c938041492842f0ca825e22875fd19fd854fab1c
unknown <1131531950@qq.com>
2018-07-15 10:58:46 +0800

    first commit

--- gin.bat
+++ gin.bat
@@ -1,6 +1,6 @@
 git init
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/Cptraser/Cptraser.git
+git remote add origin https://github.com/RandomMaker/RandomMaker.git
 git push -u origin master
 pause
